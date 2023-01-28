496bc1187cc11371719acc02e9f77b0d22999b4d
V10l3t4 <angelica.carabajal1093@gmail.com>
2023-01-28 16:39:23 -0300

    segunda escritura en el repositorio

---
+++ codigoJava.js
@@ -0,0 +1,8 @@
+document.getElementById('botont').onclick = function () {
+    console.log('capturamos el evento click');
+    document.getElementById('demo').innerHTML = "011-5-899098";
+    }
+    document.getElementById('botonc').onclick = function () {
+        console.log('capturamos el evento click');
+        document.getElementById('demo2').innerHTML = "abigail.guzman@gmail.com";
+        }
